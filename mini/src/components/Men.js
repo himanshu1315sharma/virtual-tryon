b5f057c87995631d346527e572a64756b0ee78a2
himanshu <himanshu1315sharma@gmail.com>
2023-03-19 23:08:18 +0530

    changes made

---
+++ mini/src/components/Men.js
@@ -0,0 +1,11 @@
+import React from 'react'  
+class Men extends React.Component {  
+  render() {  
+    return (  
+      <div>  
+        <h1>Home</h1>  
+      </div>  
+    )  
+  }  
+}  
+export default Men  
---
+++ mini/src/components/Women.js
@@ -0,0 +1,11 @@
+import React from 'react'  
+class Women extends React.Component {  
+  render() {  
+    return (  
+      <div>  
+        <h1>Home 2</h1>  
+      </div>  
+    )  
+  }  
+}  
+export default Wome  
